c9ffdff1276638fc5f427a9a544aa6b1ca7ec548
Nanoka <stornblood6969@gmail.com>
2024-09-05 14:14:20 -0500

    First Commit

---
+++ src/constants.ts
@@ -0,0 +1,40 @@
+export const topbarLinks = [
+  {
+    id: 1,
+    name: "Responsabilidad Social",
+    href: "/responsabilidad-social",
+  },
+  {
+    id: 2,
+    name: "Canal de denuncias",
+    href: "/canal-de-denuncias",
+  },
+  {
+    id: 3,
+    name: "Buzón de sugerencias",
+    href: "/buzon-de-sugerencias",
+  },
+  {
+    id: 4,
+    name: "Contacto",
+    href: "/contacto",
+  },
+];
+
+export const sliderItems = [
+  {
+    id: 1,
+    image: "/src/images/slider/banner-coragensc-4lt-1.jpg",
+    alt: "Coragensc 4lt",
+  },
+  {
+    id: 2,
+    image: "/src/images/slider/banner-coragensc-100ml-3.jpg",
+    alt: "Coragensc 100ml",
+  },
+  {
+    id: 3,
+    image: "/src/images/slider/banner-coragensc-10lt-4.jpg",
+    alt: "Coragensc 10lt",
+  },
+];
